7fad2ec1262e2198018df7f042657d889fd3c04f
prasadm <prasadm@Prasad.local>
2019-04-15 22:51:16 +0530

    first commit

---
+++ helloworld.java
@@ -0,0 +1,16 @@
+import java.lang.*;
+class Demo
+{
+static public void main(String args[]){
+System.out.print("hello world");
+}
+
+}
+
+class Demo2
+{
+static public void main(String args[]){
+System.out.print("hello world2");
+}
+
+}
